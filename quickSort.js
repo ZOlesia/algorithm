d1b1aae6f756051e6899fa17eb421a4b6b796b9e
Alexandra <56olesia65@gmail.com>
2018-06-08 12:27:45 -0700

    quickSort first commit

---
+++ quickSort.js
@@ -0,0 +1,22 @@
+function quickSort(arr){
+    if(arr.length<=1){
+        return arr;
+    }
+
+    var left=[];
+    var right=[];
+    var piv=arr[0];
+    for(var i = 1; i < arr.length; i++){
+        if(arr[i]>piv){
+            right.push(arr[i]);
+        } else {
+            left.push(arr[i]);
+        }
+    }
+    
+        console.log(left);
+        console.log(right);
+    return quickSort(left).concat(piv).concat(quickSort(right));
+}
+
+console.log(quickSort([5, 0, 89, 3, 100, 1]));
