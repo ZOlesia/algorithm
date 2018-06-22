d433e5a16b1d9c4a8b88e939789c4d28d54e0aef
Alexandra <56olesia65@gmail.com>
2018-06-22 13:17:07 -0700

    find second ma number in the array added

---
+++ 2ndmax.js
@@ -0,0 +1,17 @@
+function findSecondLargeNumber(arr){
+	
+	var fLargeNum = 0;
+	var sLargeNum = 0;
+	
+	for(var i=0; i<arr.length; i++){
+		if(fLargeNum < arr[i]){
+			sLargeNum = fLargeNum;
+			fLargeNum = arr[i];			
+		}else if(sLargeNum < arr[i]){
+			sLargeNum = arr[i];
+		}
+	}
+	return sLargeNum;
+}
+
+console.log(findSecondLargeNumber([15, 1, 0, 20]));
---
+++ strings.js
@@ -0,0 +1,6 @@
+function reverseSentence(str){
+    var words = str.split(" ").reverse();
+    console.log(words);
+}
+
+reverseSentence("I love you");
