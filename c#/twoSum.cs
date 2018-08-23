9d03825ccbca7039e0b48488452507036b27fd0e
Alexandra <56olesia65@gmail.com>
2018-08-23 13:22:41 -0700

    +1

---
+++ c#/twoSum.cs
@@ -0,0 +1,20 @@
+namespace c_
+{
+    public class TwoSum
+    {
+        public int[] twoSum(int[] nums, int target) {
+            for(int i = 0; i < nums.Length; i++)
+            {
+                for(int j = i+1; j < nums.Length; j++)
+                {
+                    if(nums[i] + nums[j] == target) 
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            
+            return new int[] {};
+        }
+    }
+}
