0acd215470425509e26d80527669095e7a1f72ce
Alexandra <56olesia65@gmail.com>
2019-01-09 11:53:36 -0800

    +1

--- c#/bst.cs
+++ c#/bst.cs
@@ -27,6 +27,17 @@ namespace c_
                 
                 return root;
             }
+
+            public bool IsUnivalTree(TreeNode root) {
+
+                if(root == null) return true;
+                
+                if(root.left != null && root.left.val != root.val) return false;
+                if(root.right != null && root.right.val != root.val) return false;
+                
+                return IsUnivalTree(root.left) & IsUnivalTree(root.right);
+                
+            }
         }
     }
 }
