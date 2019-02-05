702f5e44be1f06c5c15ebc0a2a794164160af65a
Alexandra <56olesia65@gmail.com>
2019-02-04 18:47:20 -0800

    +1

--- Crackin The Coding Interview/StringsArrays/Program.cs
+++ Crackin The Coding Interview/StringsArrays/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace StringsArrays
 {
     class Program
     {
-        public static bool isPermutation(string s1, string s2)
+        public static bool IsPermutation(string s1, string s2)
         {
             s1.ToLower();
             s2.ToLower();
@@ -18,7 +20,7 @@ namespace StringsArrays
             return true;
         }
 
-        public static string Urlify(string s, int l)
+         public static string Urlify(string s, int l)
         {
             var count = 0;
             for(int i = 0; i < l; i++)
@@ -47,9 +49,46 @@ namespace StringsArrays
             }
             return str.ToString();
         }
+
+        public static int[,] ZeroMatrix(int[,] arr)
+        {
+            var rows = new HashSet<int>();
+            var columns = new HashSet<int>();
+
+            for(int i = 0; i < arr.GetLength(0); i++)
+            {
+                for(int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if(arr[i, j] == 0)
+                    {
+                        rows.Add(i);
+                        columns.Add(j);
+                    }
+                }
+            }
+
+            foreach(int i in rows)
+            {
+                for(int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = 0;
+                }
+            }
+
+            foreach(int i in columns)
+            {
+                for(int j = 0; j < arr.GetLength(0); j++)
+                {
+                    arr[j, i] = 0;
+                }
+            }
+
+            return arr;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var arr = new int[4,4] { {1,3,8,0}, {2,7,1,3}, {0,2,0,1}, {6,7,9,1} };
+            Console.WriteLine(ZeroMatrix(arr));
         }
     }
 }
