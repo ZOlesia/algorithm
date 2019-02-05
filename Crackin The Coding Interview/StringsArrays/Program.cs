64124e5fe4f176a51c7f14568ff6cb6a350c0a5e
Alexandra <56olesia65@gmail.com>
2019-02-05 10:54:32 -0800

    +1

--- Crackin The Coding Interview/StringsArrays/Program.cs
+++ Crackin The Coding Interview/StringsArrays/Program.cs
@@ -85,10 +85,47 @@ namespace StringsArrays
 
             return arr;
         }
+
+        public static bool StringRotation(string str1, string str2)
+        {
+            if(str1.Length != str2.Length) return false;
+            var str = str1 + str1;
+
+            return str.Contains(str2);
+        }
+
+        public static int[][] RotateMatrix(int[][] matrix)
+        {
+            if(matrix.Length != matrix[0].Length) return matrix;
+            for(int i = 0; i < matrix.Length/2; i++)
+            {
+                int first = i;
+                int last = matrix.Length - 1 - i;
+                for(int j = first; j < last; j++)
+                {
+                    int offset = j - first;
+                    int top = matrix[first][j]; //saving top value
+
+                    //moving left value to the top 
+                    matrix[first][j] = matrix[last-offset][first];
+
+                    //moving bottom value to the left
+                    matrix[last-offset][first] = matrix[last][last-offset];
+
+                    //moving right value to the bottom
+                    matrix[last][last-offset] = matrix[j][last];
+
+                    //moving top value to the right 
+                    matrix[j][last] = top;
+                }
+            }
+            return matrix;
+        }
         static void Main(string[] args)
         {
-            var arr = new int[4,4] { {1,3,8,0}, {2,7,1,3}, {0,2,0,1}, {6,7,9,1} };
-            Console.WriteLine(ZeroMatrix(arr));
+            // var arr = new int[4,4] { {1,3,8,0}, {2,7,1,3}, {0,2,0,1}, {6,7,9,1} };
+            // Console.WriteLine(ZeroMatrix(arr));
+            System.Console.WriteLine(StringRotation("waterbottle", "erbottlewat"));
         }
     }
 }
