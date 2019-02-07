e33424751b94c48a2c30e0f9cd5a81df2bb2aefc
Alexandra <56olesia65@gmail.com>
2019-02-06 18:46:37 -0800

    +1

--- Crackin The Coding Interview/LL/Program.cs
+++ Crackin The Coding Interview/LL/Program.cs
@@ -75,46 +75,76 @@ namespace LL
         //         runner = runner.next;
         //     }
         //     return head;
-        // }
-        public static LinkedListNode SumLists(LinkedListNode head1, LinkedListNode head2){
-            //get the longest length of the lists
-            //I will assume head1 has the longest length
-            var r1 = head1;
-            var r2 = head2;
+        // // }
+        // public static LinkedListNode SumLists(LinkedListNode head1, LinkedListNode head2){
+        //     //get the longest length of the lists
+        //     //I will assume head1 has the longest length
+        //     var r1 = head1;
+        //     var r2 = head2;
             
-            while(r1!=null){
-                var increaser = 0;
-                var num2 = 0;
-                if(r2 != null) num2 = r2.Value;
+        //     while(r1!=null){
+        //         var increaser = 0;
+        //         var num2 = 0;
+        //         if(r2 != null) num2 = r2.Value;
 
-                var sum = r1.Value + num2;
+        //         var sum = r1.Value + num2;
 
-                if(sum > 9){
-                    var splitNum = GetIntArray(sum);
-                    increaser = splitNum[1];
-                    r1.Next.Value += splitNum[1];
+        //         if(sum > 9){
+        //             var splitNum = GetIntArray(sum);
+        //             increaser = splitNum[1];
+        //             r1.Next.Value += splitNum[1];
+        //         }
+
+        //         if(increaser == 0 && r2 == null) return r1;
+        //         if(r2 != null) r2 = r2.Next;
+
+        //         r1 = r1.Next;
+        //     }
+        //     return r1;
+        // }
+        // public static List<int> GetIntArray(int num)
+        // {
+        //     List<int> listOfInts = new List<int>();
+        //     while(num > 0)
+        //     {
+        //         listOfInts.Add(num % 10);
+        //         num = num / 10;
+                
+        //     }
+        //     return listOfInts.Reverse();
+        // }
+
+        public static bool Palindrome(LinkedListNode head){
+            var faster = head;
+            var current = head;
+            var isOdd = false;
+            var arr = new List<int>();
+            
+            while(faster != null){
+
+                if(faster.Next == null){
+                    break;
                 }
 
-                if(increaser == 0 && r2 == null) return r1;
-                if(r2 != null) r2 = r2.Next;
+                arr.Add(current.Value);
 
-                r1 = r1.Next;
+                faster = faster.Next.Next;
+                current = current.Next;
             }
-            return r1;
-        }
-        public static List<int> GetIntArray(int num)
-        {
-            List<int> listOfInts = new List<int>();
-            while(num > 0)
-            {
-                listOfInts.Add(num % 10);
-                num = num / 10;
-                
+
+            if(isOdd) current = current.Next;
+            
+
+            for(int i = arr.Count; i >= 0; i--){
+                if(arr[i] != current.Value) return false;
+                current = current.Next;
             }
-            return listOfInts.Reverse();
+            return true;
         }
+
         static void Main(string[] args)
         {
+            System.Console.WriteLine(5/2);
             // var num = 26;
             // GetIntArray(num);
             // Console.WriteLine("Hello World!");
