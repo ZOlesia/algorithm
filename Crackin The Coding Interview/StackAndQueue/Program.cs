eef173e054926b5a347b368df8a2ef2d129a66e9
ZOlesia <56olesia65@gmail.com>
2019-02-11 13:38:08 -0800

    +1

---
+++ Crackin The Coding Interview/StackAndQueue/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        public static void sortStack(Stack<int> s)
+        {
+            Stack<int> tempStack = new Stack<int>();
+            while(s.Count != 0)
+            {
+                var min = s.Pop();
+
+                while(tempStack.Count != 0 && tempStack.Peek() > min)
+                {
+                    s.Push(tempStack.Pop());
+                }
+
+                tempStack.Push(min);
+            }
+
+            while(tempStack.Count != 0)
+            {
+                s.Push(tempStack.Pop());
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
