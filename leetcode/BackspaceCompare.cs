1e6f4adb9a0e228f6e3e31a1c47b7834a39a0048
Olesia Zadereiko (HCL America Inc) <v-olzad@microsoft.com>
2019-02-25 14:44:09 -0800

    +1

--- leetcode/BackspaceCompare.cs
+++ leetcode/BackspaceCompare.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Text;
 
 namespace leetcode
@@ -34,6 +35,38 @@ namespace leetcode
             }
             
         return s.ToString();  
+        }
+
+        //other way of using Stack 
+        public static bool backspaceCompareUsingStack(string S, string T){
+            var st1 = new Stack<char>();
+            var st2 = new Stack<char>();
+
+            for(int i = 0; i < S.Length; i++){
+                if(S[i] != '#'){
+                    st1.Push(S[i]);
+                } else if(st1.Count != 0){
+                    st1.Pop();
+                }
+            }
+
+            for(int i = 0; i < T.Length; i++){
+                if(T[i] != '#'){
+                    st2.Push(T[i]);
+                } else if(st2.Count != 0){
+                    st2.Pop();
+                }
+            }
+
+            if(st1.Count != st2.Count) return false;
+
+            while(st1.Count != 0){
+                var current = st1.Pop();
+                if(st2.Count == 0 || st2.Pop() != current){
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
