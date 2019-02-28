c41d8bf96baa77ea34bfdc15798c8c03648d27ff
Olesia Zadereiko (HCL America Inc) <v-olzad@microsoft.com>
2019-02-27 16:35:11 -0800

    +1

--- leetcode/IntersectionOfTwoArrays.cs
+++ leetcode/IntersectionOfTwoArrays.cs
@@ -1,7 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace leetcode
 {
     public class IntersectionOfTwoArrays
     {
-        
+        public static int[] Intersection(int[] nums1, int[] nums2) {
+            var set = new HashSet<int>();
+            
+            for(int i = 0; i < nums1.Length; i++){
+                set.Add(nums1[i]);
+            }
+            
+            var resultSet = new HashSet<int>();
+            
+            for(int i = 0; i < nums2.Length; i++){
+                if(set.Contains(nums2[i])){
+                    resultSet.Add(nums2[i]);
+                }
+            }
+            
+            
+            
+            return resultSet.ToArray();
+        }
     }
 }
